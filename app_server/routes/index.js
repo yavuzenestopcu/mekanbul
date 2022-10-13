3b99503f28863dce3c6069ab70bed7796ac0e687
yavuzenestopcu <yavuz.enes.topcu@protonmail.com>
2022-10-13 11:12:40 +0300

    13.10 Ders

--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,8 +1,12 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main.js')
+var ctrlMekanlar = require('../controllers/mekanlar.js')
+var ctrlDigerleri = require('../controllers/digerleri.js')
 
 /* GET home page. */
-router.get('/', ctrlMain.index);
+router.get('/', ctrlMekanlar.anaSayfa);
+router.get('/mekan', ctrlMekanlar.mekanBilgisi);
+router.get('/mekan/yorum/yeni', ctrlMekanlar.yorumEkle);
+router.get('/hakkinda', ctrlDigerleri.hakkinda);
 
 module.exports = router;
