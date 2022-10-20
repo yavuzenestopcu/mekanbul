ac16f9a2f33b134d86ece93d043c64b5c367ed04
yavuzenestopcu <yavuz.enes.topcu@protonmail.com>
2022-10-20 12:36:55 +0300

    odev3

--- app_server/controllers/mekanlar.js
+++ app_server/controllers/mekanlar.js
@@ -2,11 +2,76 @@ var express = require('express');
 var router = express.Router();
 
 const anaSayfa=function(req, res, next) {
-    res.render('anasayfa', { title: 'Anasayfa' });
+    res.render('anasayfa', { 
+      'baslik': 'Anasayfa',
+      'sayfaBaslik':{
+        'siteAd': 'MekanBul',
+        'slogan': 'Civardaki Mekanları Keşfet'
+      },
+      'mekanlar': [
+        {
+          'ad': 'Starbucks',
+          'puan': 4,
+          'adres': 'Centrum Garden AVM',
+          'imkanlar': ['Dünya Kahveleri','Kek','Pasta'],
+          'mesafe': '10km'
+        },
+        {
+          'ad': 'Barida Cam Kafe',
+          'puan': 5,
+          'adres': 'SDÜ Batı Kampüsü',
+          'imkanlar': ['Mühendis Gençler', 'Çay', 'Kahve', 'Tost'],
+          'mesafe': '1km'
+        },
+        {
+          'ad': 'Popeyes',
+          'puan': 3,
+          'adres': 'İyaşpark AVM',
+          'imkanlar': ['Tavuk', 'Burger', 'İçecek'],
+          'mesafe': '1km'
+        }
+      ]   
+    
+    });
 }
 
 const mekanBilgisi=function(req, res, next) {
-    res.render('mekanbilgisi', { title: 'Mekan Bilgisi' });
+    res.render('mekanbilgisi', { 
+      'baslik': 'Mekan Bilgisi',
+      'mekanBaslik': 'Starbucks',
+      'mekanDetay': {
+        'ad': 'Starbucks',
+        'puan': 4,
+        'adres': 'Centrum Garden AVM',
+        'saatler': [
+          {
+            'gunler': 'Pazartesi-Cuma',
+            'acilis': '07:00',
+            'kapanis': '23:00',
+            'kapali': false
+          },
+          {
+            'gunler': 'Cumartesi-Pazar',
+            'acilis': '09:00',
+            'kapanis': '22:00',
+            'kapali': false
+          }
+        ],
+        'imkanlar': ['Dünya Kahveleri', 'Kek', 'Pasta'],
+        'koordinatlar': {
+          'enlem': 37.78,
+          'boylam': 30.56
+        },
+        'yorumlar': [
+          {
+            'yorumYapan': 'Umut Selek',
+            'yorumMetni': 'Mükemmel bir iş yeri. Güler yüzlü, tatlı dilli çalışanları var. Hijyenik bir yer.',
+            'tarih': '8 Ağustos 2022',
+            'puan': 4
+          }
+        ]
+      }     
+    });
 }
 
 const yorumEkle=function(req, res, next) {
