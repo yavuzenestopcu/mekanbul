320c1e800589f731f6bbf140beccbb7e4b6320e8
Yavuz Enes Topçu <Yavuz.enes.topcu@protonmail.com>
2022-11-20 23:28:02 +0300

    odev5

--- app_api/controllers/yorumlar.js
+++ app_api/controllers/yorumlar.js
@@ -17,7 +17,48 @@ const yorumGuncelle = function (req, res) {
 }
 
 const yorumGetir = function (req, res) {
-    cevapOlustur(res, 200, { "durum": "başarılı" })
+    if (req.params && req.params.mekanid && req.params.yorumid) {
+        Mekan.findById(req.params.mekanid)
+            .select("ad yorumlar")
+            .exec(function (hata, mekan) {
+                var cevap, yorum
+                if (!mekan) {
+                    cevapOlustur(res, 404, {
+                        "hata": "Böyle bir mekan yok!"
+                    })
+                    return
+                } else if (hata) {
+                    cevapOlustur(res, 404, hata)
+                    return
+                }
+                if (mekan.yorumlar && mekan.yorumlar.length > 0) {
+                    yorum = mekan.yorumlar.id(req.params.yorumid)
+                    if (!yorum) {
+                        cevapOlustur(res, 404, {
+                            "hata": "Böyle bir yorum yok!"
+                        })
+                    }
+                    else {
+                        cevap = {
+                            mekan: {
+                                ad: mekan.ad,
+                                id: req.params.mekanid
+                            },
+                            yorum: yorum
+                        }
+                        cevapOlustur(res, 200, cevap)
+                    }
+                } else {
+                    cevapOlustur(res, 404, {
+                        "hata": "Hiç yorum yok"
+                    })
+                }
+            })
+    } else {
+        cevapOlustur(res, 404, {
+            "hata": "Bulunamadı. mekanid ve yorumid mutlaka girilmeli."
+        })
+    }
 }
 
 module.exports = {
